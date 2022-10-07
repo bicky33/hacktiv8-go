ae740701c6451be42edae50f4b57f17c6176bf0a
bicky33 <biki.hamzah@gmail.com>
2022-10-07 12:41:46 +0700

    skip assignment-3

---
+++ assignment-3/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Status struct {
+	Wind  int `json:"wind"`
+	Water int `json:"water"`
+}
+
+type Response struct {
+	Status Status `json:"status"`
+}
+
+func GenerateRandomNumber() int {
+	min := 1
+	max := 100
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	// go func() {
+	// 	tick := time.Tick(time.Second * 1)
+	// 	for range tick {
+	// 		StatusWrite()
+	// 	}
+	// }()
+
+	server := http.Server{
+		Addr: ":8080",
+	}
+	server.ListenAndServe()
+}
+
+func StatusWrite() {
+	data := Status{Wind: GenerateRandomNumber(), Water: GenerateRandomNumber()}
+	response := Response{Status: data}
+	fileName := "./file/status.json"
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	encode := json.NewEncoder(file)
+	encode.Encode(response)
+	defer file.Close()
+}
